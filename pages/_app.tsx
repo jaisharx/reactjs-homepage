8b9d8bc853e6e7ffdef26b61099d8fc8989480da
jai-sharma <sendtojsharma@gmail.com>
2021-10-06 17:38:25 +0000

    adding ChakraProvider and GlobalCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalCSS from 'styles/globals'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <GlobalCSS />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 export default MyApp
