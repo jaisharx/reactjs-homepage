0085c4a35f84348b8f417ee3c27ed85e67909b2a
jai-sharma <sendtojsharma@gmail.com>
2021-10-09 12:24:07 +0000

    adding chakra theme to store color values

---
+++ styles/theme.tsx
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      400: '#61dafb',
+      500: '#20232A',
+    },
+    shades: {
+      400: '#373940'
+    }
+  },
+})
+
+export default theme
