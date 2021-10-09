f7046333654940ecc999e84463a611d29fc262d1
jai-sharma <sendtojsharma@gmail.com>
2021-10-09 12:24:19 +0000

    passing theme to chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
 import GlobalCSS from 'styles/globals'
+import theme from 'styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <GlobalCSS />
       <Component {...pageProps} />
     </ChakraProvider>
